7c2bbcd076a26fa8b1690639f330ee0f5c4e8603
616385187 <616385187@qq.com>
2018-11-21 13:53:46 +0800

    2018年11月21日13:50:50 webpack 生成 bundle 文件 如果没有npx 命令参照： webpack-cli [options] --entry <entry> --output <output> 本项目为：G:\vueproject\v\node_modules\.bin>webpack ../../src/index.js --output ../../dist/main.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import _ from 'lodash';
+
 function compent() {
     var element =document.createElement('div');
+    // +   // Lodash, now imported by this script
     element.innerHTML = _.join(['hello','webpack'],'');
     return element;
 }
