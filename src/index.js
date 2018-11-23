27cff70c1ed362748147ffea30931844390c0af8
616385187 <616385187@qq.com>
2018-11-23 10:43:35 +0800

    // 需要编译器，即完整版： new Vue({   template: '<div>{{ hi }}</div>' }) ***************************************** webpack module.exports = {   // ...   resolve: {     alias: {       'vue$': 'vue/dist/vue.esm.js' // 用 webpack 1 时需用 'vue/dist/vue.common.js'     }   } }

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import _ from 'lodash';
+import Vue from 'vue'
 
-function compent() {
-    var element =document.createElement('div');
-    // +   // Lodash, now imported by this script
-    element.innerHTML = _.join(['hello','webpack'],'');
-    return element;
-}
-document.body.appendChild(compent())
+var vm = new Vue({
+    el: '#vue_det',
+    template:
+    '<div><h1>site : {{site}}</h1><h1>url : {{url}}</h1><h1>{{details()}}</h1></div>' ,
+    data: {
+        site: "菜鸟教程",
+        url: "www.runoob.com",
+        alexa: "10000"
+    },
+    methods: {
+        details: function() {
+            return  this.site + " - 学的不仅是技术，更是梦想！";
+        }
+    },
+})
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
+const {VueLoaderPlugin} = require('vue-loader');
 module.exports = {
     entry:__dirname+'/src/index.js',// __dirname 或者 ../..
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            //解析vue后缀文件
+            {test: /\.vue$/, loader: 'vue-loader'},
+            //用巴babel解析js文件 排除模块安装目录的文件
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js' // 用 webpack 1 时需用 'vue/dist/vue.common.js'
+        }
     }
 }
